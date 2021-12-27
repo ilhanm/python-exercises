3f293a31907fd10db7d4e90d94b8b8ab2e3e97a8
Ilhan Mert <ilhanmertalan@gmail.com>
2021-12-27 18:59:05 +0300

    first commit

---
+++ csvtojson_of_all_instances/Products.py
@@ -0,0 +1,50 @@
+#########27.12.2021#########
+##1] my products class has 3 main attributes which is name, price, quantity
+##2] i gave my program a csv file for creating instances by reading it from there
+##3] then i take output of all my objects formatted in json
+#########
+
+import csv
+import json
+class Product:
+    payment_rate=0.8 #class attribute
+    productlist=[]  #to hold all my product in one list 
+    def __init__(self, name: str, price: float, quantity=0) -> None:
+        #Run validations to the received args
+        assert price>0, "price should be greater than zero" #some assertions for validation
+        assert quantity>=0, "quantity should not be less than zero"
+        
+        #assigning values to object
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+        Product.productlist.append(self)    #add each created instance to product list
+
+    def calculate_total_price(self):
+        return self.quantity*self.price
+
+    def apply_discount(self):
+        self.price=self.price*self.payment_rate
+    
+    def __repr__(self) -> str:
+        return f"[name:'{self.name}', price: {self.price}, quantity: {self.quantity}]"
+    
+    def toJSON(self):   #got this code piece from stackoverflow
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+    
+with open('csvtojson_of_all_instances/products.csv') as csv_file:
+    csv_reader = list(csv.reader(csv_file, delimiter=','))
+    for item in csv_reader[1:]:
+        myprod=Product(item[0],int(item[1]),int(item[2]))
+newproduct=Product("telefon",1230,22)
+
+
+with open("csvtojson_of_all_instances/output.json", "w") as outfile:
+    outfile.write(Product.toJSON(Product.productlist))  #converting list of my objects to json format
+    
+    
+    
+
+
+
