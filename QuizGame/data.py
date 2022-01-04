05a80b5aa382aa9f450390677ec01cbb14f8de95
Ilhan Mert <ilhanmertalan@gmail.com>
2022-01-04 20:49:16 +0300

    day17

---
+++ QuizGame/data.py
@@ -0,0 +1,101 @@
+question_data= [{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "FLAC stands for 'Free Lossless Audio Condenser'",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "hard",
+    "question": "DHCP stands for Dynamic Host Configuration Port.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "'HTML'; stands for Hypertext Markup Language.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1;'.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "hard",
+    "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "Time on Computers is measured via the EPOX System.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "A Boolean value of '0' represents which of these words?",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "easy",
+    "question": "The Python programming language gets its name from the British comedy group 'Monty Python';",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+}
+]
